30510fca3803e8ecab389fb1a2b31f4a026a7c52
Jason Wihardja <jason.wihardja@gmail.com>
2015-04-25 17:37:28 +0700

    Optimize Algorithm

--- NodeJSApp/addons/PrimeJS.js
+++ NodeJSApp/addons/PrimeJS.js
@@ -7,6 +7,7 @@
                 for (var j = 2; j < i; j++) {
                     if (i % j == 0) {
                         isPrime = false;
+                        break;
                     }
                 }
             }
--- PrimeCPP/src/prime.cpp
+++ PrimeCPP/src/prime.cpp
@@ -24,6 +24,7 @@ void FindPrime(const FunctionCallbackInfo<Value>& args) {
 					for (int j = 2; j < i; j++) {
 						if (i % j == 0) {
 							isPrime = false;
+							break;
 						}
 					}
 				}
